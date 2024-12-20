e9173e657dd2f1619dd5648e59f96b755274f867
Nely Victória Oliveira da Silva <o.nely@escolar.ifrn.edu.br>
2024-12-20 16:23:15 -0300

    20 do 12

---
+++ pjdnovo/Assets/Scenes/new.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Transform _transform;
+    public float velocidade = 10f
+    void Start()
+    {
+        _transform = gameObject.transform;
+    }
+
+    void Update()
+    {
+        if(input.GetKey(KeyCode.LeftArrow))
+        {
+            _transform.position=
+            Debug.Log("LeftArrow");
+        }
+
+        if(input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("RightArrow");
+        }
+    }
+}
